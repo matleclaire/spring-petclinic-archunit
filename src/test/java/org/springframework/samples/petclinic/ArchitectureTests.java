b751f85d94cbe231c8997e8c56f8fcb4879b5aa5
Mathieu Leclaire <mat.leclaire@gmail.com>
2024-10-13 19:23:55 +0200

    Add ArchRule for logging Beta

--- src/test/java/org/springframework/samples/petclinic/ArchitectureTests.java
+++ src/test/java/org/springframework/samples/petclinic/ArchitectureTests.java
@@ -1,17 +1,28 @@
 package org.springframework.samples.petclinic;
 
-import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.core.domain.JavaMethod;
+import com.tngtech.archunit.core.domain.JavaMethodCall;
 import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.CompositeArchRule;
+import com.tngtech.archunit.lang.ConditionEvents;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.logging.Logger;
 
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
+import static com.tngtech.archunit.lang.ConditionEvent.createMessage;
+import static com.tngtech.archunit.lang.SimpleConditionEvent.satisfied;
+import static com.tngtech.archunit.lang.SimpleConditionEvent.violated;
 import static com.tngtech.archunit.lang.conditions.ArchConditions.dependOnClassesThat;
 import static com.tngtech.archunit.lang.conditions.ArchConditions.not;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
 
 @AnalyzeClasses(packagesOf = PetClinicApplication.class, importOptions = DoNotIncludeTests.class)
 class ArchitectureTests {
@@ -35,5 +46,30 @@ class ArchitectureTests {
 			classes()
 				.that().resideInAPackage("..vet..")
 				.should(not(dependOnClassesThat(resideInAPackage("..owner..")))));
+
+
+	@ArchTest
+	public static final ArchRule CONTROLLER_SHOULD_LOG = methods()
+		.that().areAnnotatedWith(PostMapping.class)
+		.should(log());
+
+
+	private static ArchCondition<? super JavaMethod> log() {
+		return new ArchCondition<>("log") {
+			@Override
+			public void check(JavaMethod item, ConditionEvents events) {
+				List<JavaMethodCall> loggingCalls = item.getMethodCallsFromSelf().stream()
+					.filter(call -> call.getTargetOwner().isEquivalentTo(Logger.class))
+					.toList();
+				if (loggingCalls.isEmpty()) {
+					events.add(violated(item, createMessage(item, "does not log")));
+				} else {
+					for (JavaMethodCall loggingCall : loggingCalls) {
+						events.add(satisfied(item, loggingCall.getDescription()));
+					}
+				}
+			}
+		};
+	}
 
 }
